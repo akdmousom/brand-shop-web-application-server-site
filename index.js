ee2280ebdb1032cb9768454a91878adfe607c721
AKD <contact.mousom@gmail.com>
2023-10-18 14:14:05 +0600

    MongoDB connected and all username and password using from .env file

---
+++ index.js
@@ -0,0 +1,58 @@
+// Dependencys
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// Initialize App
+const app = express();
+
+
+
+// App EndPoint
+
+app.get('/', (req,res)=>{
+    res.send('Server Is Running')
+})
+
+
+// Database connection 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.chkrm7d.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.connect();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+// App listening
+app.listen(port, ()=>{
+    console.log(`App listen on port ${port}`);
+})
