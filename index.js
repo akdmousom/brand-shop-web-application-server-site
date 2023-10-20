242658e77f2f620b4e60852442e2e10b0f209a50
AKD <contact.mousom@gmail.com>
2023-10-20 19:09:57 +0600

    Backend done

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.json())
 
 // App EndPoint
 
-app.get('/', (req,res)=>{
-    res.send('Server Is Running')
+app.get('/', (req, res) => {
+  res.send('Server Is Running')
 })
 
 
@@ -36,87 +36,118 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    const database = client.db("brand-shop");
-    const users = database.collection("users"); 
-    const brands = database.collection("brands"); 
-    const products = database.collection("products"); 
-    const addToCart = database.collection("addToCart"); 
+  const database = client.db("brand-shop");
+  const users = database.collection("users");
+  const brands = database.collection("brands");
+  const products = database.collection("products");
+  const addToCart = database.collection("addToCart");
 
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    app.get('/users',async (req,res)=>{
+    app.get('/users', async (req, res) => {
 
-        const cursor = await users.find().toArray()
+      const cursor = await users.find().toArray()
 
-        res.send(cursor)
+      res.send(cursor)
     })
 
-    app.post('/users', async (req,res)=>{
-        const doc = req.body;
-        const result = await users.insertOne(doc)
-        res.send(result)
-        
+    app.post('/users', async (req, res) => {
+      const doc = req.body;
+      const result = await users.insertOne(doc)
+      res.send(result)
+
     })
 
-    app.post('/brands', async (req, res)=> {
+    app.post('/brands', async (req, res) => {
       const doc = req.body;
       const result = await brands.insertOne(doc);
       res.send(result)
-     
+
     })
 
-    app.get('/brands', async(req, res)=>{
+    app.get('/brands', async (req, res) => {
       const cursor = await brands.find().toArray()
       res.send(cursor)
     })
 
-    app.get('/single-brands/:id', async(req,res)=>{
+    app.get('/single-brands/:id', async (req, res) => {
       const id = req.params.id;
       const cursor = await brands.findOne(new ObjectId(id))
       res.send(cursor)
-      console.log(id);
     })
 
     app.post('/products', async (req, res) => {
       const doc = req.body;
       const result = await products.insertOne(doc)
       res.send(result)
-      
+
     })
 
-    app.get('/products', async(req, res)=> {
+    app.get('/products', async (req, res) => {
       const cursor = await products.find().toArray();
       res.send(cursor)
     })
 
-    app.get('/product-details/:id', async(req,res)=>{
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const query = await products.findOne(filter)
+      const option = { upsert: true }
+      const doc = req.body
+      const updatedDoc = {
+        $set: {
+
+          productImg: doc.productImg,
+          productName: doc.productName,
+          brandName: doc.brandName,
+          productType: doc.productType,
+          productPrice: doc.productPrice,
+          shortDescription: doc.shortDescription,
+          productRating: doc.productRating
+
+        }
+      }
+      const result = await products.updateOne(filter, updatedDoc, option)
+      console.log(doc);
+      res.send(result)
+    })
+
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const cursor = await products.findOne(query)
+      res.send(cursor)
+    })
+
+    app.get('/product-details/:id', async (req, res) => {
       const id = req.params.id;
       const cursor = await products.findOne(new ObjectId(id))
       res.send(cursor)
     })
 
-    app.post('/add-to-cart', async(req,res) => {
+
+    app.post('/add-to-cart', async (req, res) => {
       const doc = req.body;
       const result = await addToCart.insertOne(doc);
       res.send(result);
-    } )
+    })
 
-    app.get('/cart', async(req,res)=>{
+    app.get('/cart', async (req, res) => {
       const doc = await addToCart.find().toArray();
       res.send(doc)
     })
 
-    app.delete('/cart/:id', async(req,res)=> {
+    app.delete('/cart/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await addToCart.deleteOne(query)
       res.send(result)
     })
 
-    
+
 
 
 
@@ -139,6 +170,6 @@ run().catch(console.dir);
 
 
 // App listening
-app.listen(port, ()=>{
-    console.log(`App listen on port ${port}`);
+app.listen(port, () => {
+  console.log(`App listen on port ${port}`);
 })
